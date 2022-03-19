fad234cd80b8e22bf89e25b6c962fb2e64d39dec
Baran Yeni <info@baranyeni.com>
2022-03-19 08:14:53 +0300

    Translations fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask_babelex import Babel
+from flask import Flask, render_template
+from flask_babel import Babel
 from flask_migrate import Migrate
 
 from app.models import db
@@ -12,8 +12,8 @@ from app.routes.ShopRouter import ShopRouter
 # -- Configuration -- #
 app = Flask(__name__,
             static_url_path='/assets',
-            static_folder='web/static',
-            template_folder='web/templates')
+            static_folder='../web/static',
+            template_folder='../web/templates')
 
 app.config.from_pyfile('../config/project.py')
 app.register_blueprint(ShopRouter, url_prefix='/shops')
@@ -34,3 +34,8 @@ setup_admin(app)
 @babel.localeselector
 def get_locale():
     return 'en'
+
+
+@app.route('/')
+def index():
+    return render_template("app/index/index.html")
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,6 @@
 # This file was created to run the application on heroku using gunicorn.
 # Read more about it here: https://devcenter.heroku.com/articles/python-gunicorn
-
-from flask import render_template
 from app import app
-
-
-@app.route('/')
-def index():
-    return render_template("app/index/index.html")
-
 
 if __name__ == '__main__':
     app.run()
